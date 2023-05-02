b6b4a273c4e0a23ae36ee6125b0a49810c38430e
Omar <57999049+ahmedmoutaalim@users.noreply.github.com>
2023-05-02 15:54:06 +0100

    fin multi lang task

--- redisign/src/component/layouts/dir.js
+++ redisign/src/component/layouts/dir.js
@@ -1,21 +1,20 @@
 
 import React,{useState} from 'react'
-
-const dir = () => {
-
+import { useTranslation } from 'react-i18next'
+const Dir = () => {
+const [t,i18n] = useTranslation()
     return (
         <>
 			{/* <!-- Service --> */}
 			<section className="content-inner">
                 
 				<div className="container">
-                    <h2>الأقسام/الادارة التنفيدية/رئيس مجلس الادارة</h2>
-                    <h2 className='clr3'>الاستاذ: محمد حمد الناصر </h2>
+                    <h2>{t("dirTitle")}</h2>
+                    <h2 className='clr3'> {t("dirp")}  </h2>
 					<div className="section-head style-3 text-center mb-4">
-						<h5 className="title">يمتلك خبرة تزيد عن 30 سنة في المجال التجاري واإلداري حاصل على البكالوريوس 
-في العلوم العسكرية ومدير تنفيذي في عدة شركات وله العديد من االستثمارات في 
-المجال التجاري و التطوير العقاري والصناعي وشريك في عدة شركات </h5>
-						<div className="dlab-separator style-2 bg-primary"></div>
+					<div className="dlab-separator style-2 bg-primary"></div>
+						<h5 className="title">{t("dirContent")} </h5>
+						
 					</div>
 
 					
@@ -29,4 +28,4 @@ const dir = () => {
     )
 }
 
-export default dir
+export default Dir
--- redisign/src/component/layouts/goals.js
+++ redisign/src/component/layouts/goals.js
@@ -1,7 +1,11 @@
 import React,{useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import img56 from '../../images/img56.png'
+import {useTranslation} from 'react-i18next'
+
+
 const Goals = () => {
+    const [t , i18n] = useTranslation();
     const [open, setOpen] = useState("p2")
     return (
         <>
@@ -9,12 +13,9 @@ const Goals = () => {
 			<section className="content-inner">
                 
 				<div className="container">
-                    <h2>الأقسام/أهدافنا</h2>
+                    <h2>{t("goalsTitle")}</h2>
 					<div className="section-head style-3 text-center mb-4">
-						<h5 className="title">دعم القطاع العسكري واألمني باإلمكانات المتاحة، ودفع عجلة التنمية بشكل مباشر والمساهمة 
-في زيادة الناتج المحلي اإلجمالي بحلول العام 2030م، وذلك لتعزز قدرات القوات المسلحة 
-واألجهزة األمنية في المملكة من استقالليتها وجاهزيتها ، من خالل المساعدة في بناء قطاع 
-صناعات عسكرية وأمنية محلية</h5>
+						<h5 className="title">{t("goalsContent")}</h5>
 						<div className="dlab-separator style-2 bg-primary"></div>
 					</div>
 
